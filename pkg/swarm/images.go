1ad7f8c6a84230acafb5eb2a0c0eed8a55776eb1
wwqdrh <huiloademail@163.com>
2023-08-01 14:47:13 +0000

    fix

--- pkg/swarm/images.go
+++ pkg/swarm/images.go
@@ -5,10 +5,12 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"os"
 	"path"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"github.com/pkg/errors"
 	"github.com/wwqdrh/gokit/ostool"
 )
 
@@ -79,4 +81,49 @@ func ImageDelete(name string, opts types.ImageRemoveOptions) ([]types.ImageDelet
 func ImageBuild(dockerfile string, imageName string) error {
 	workdir := path.Dir(dockerfile)
 	return ostool.RunCmdStd(fmt.Sprintf("cd %s; docker build -t %s .", workdir, imageName))
+}
+
+type ImageFileEntry struct {
+	Source string
+	Target string
+}
+
+func ImageBuildFromStr(content string, imageName string, entrys []ImageFileEntry) error {
+	dir, err := os.MkdirTemp("", "edge_board_build_")
+	if err != nil {
+		return errors.Wrapf(err, "创建临时文件夹失败")
+	}
+	defer func() { os.RemoveAll(dir) }()
+
+	if err := os.WriteFile(path.Join(dir, "Dockerfile"), []byte(content), os.ModePerm); err != nil {
+		return errors.Wrapf(err, "创建Dockerfile失败")
+	}
+
+	for _, item := range entrys {
+		_, err := copyFile(path.Join(dir, item.Target), item.Source)
+		if err != nil {
+			return errors.Wrapf(err, "复制文件失败")
+		}
+	}
+
+	return ImageBuild(path.Join(dir, "Dockerfile"), imageName)
+}
+
+func copyFile(dstFileName string, srcFileName string) (written int64, err error) {
+	srcFile, err := os.Open(srcFileName)
+	if err != nil {
+		return -1, errors.Wrapf(err, "open file err")
+	}
+	defer srcFile.Close()
+
+	if err := os.MkdirAll(path.Dir(dstFileName), os.ModePerm); err != nil {
+		return -1, err
+	}
+	dstFile, err := os.OpenFile(dstFileName, os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		return -1, errors.Wrapf(err, "open file err")
+	}
+	defer dstFile.Close()
+
+	return io.Copy(dstFile, srcFile)
 }
